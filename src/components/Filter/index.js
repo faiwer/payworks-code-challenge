fed810cf96647c1d030c16a8ed87c1f799e3d11f
faiwer <faiwer@gmail.com>
2019-01-26 15:43:35 +0600

    filters: refactor (get rid of redux, use router)

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import Search from './Search';
+import Language from './Language';
+
+const OrganizationFilters = () =>
+	<>
+		<Search/>
+		<Language/>
+	</>;
+const RepositoryFilters = () => <Search/>;
 
 export default () =>
 	<div className="cch-filter">
-		<Search/>
+		<Route
+			exact
+			path="/"
+			component={OrganizationFilters}
+		/>
+		<Route
+			exact
+			path="/org/:organizationName"
+			component={OrganizationFilters}
+		/>
+		<Route
+			exact
+			path="/rep/:organizationName/:repositoryName"
+			component={RepositoryFilters}
+		/>
 	</div>;
 
-// todo: filter language
 // todo: sort by: rank || number of forks
--- src/redux/filters/actions.js
+++
@@ -1,7 +0,0 @@
-export const SET_FILTER = 'FILTERS_SET';
-export const aSetFilter = (field, value) => (
-	{
-		type: SET_FILTER,
-		field,
-		value
-	});
